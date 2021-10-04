6eaf5751192a969299655b8b3f5f251de7b46f56
Igor Franco <igoretofranco@gmail.com>
2021-10-04 13:49:17 -0300

    adicionando blog pessoal

---
+++ Blog Pessoal/main.js
@@ -0,0 +1,9 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu() {
+    const btnMobile = document.getElementById('nav'); 
+    nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+
